e5e0d255df552ec63221148bc22b46e922562a32
Jot Kaur <jotkaur@cu-engr2-1-13-10.201.48.42.int.colorado.edu>
2021-04-30 11:30:06 -0600

    init

--- data/oasis_EDA.py
+++ data/oasis_EDA.py
@@ -2,8 +2,6 @@
 # %%
 # https://docs.microsoft.com/en-us/visualstudio/liveshare/use/vscode
 import pathlib
-from numpy import testing
-from numpy.fft.helper import fftfreq
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,8 +11,6 @@ import typing
 import seaborn as sns
 from jupyterthemes import jtplot
 jtplot.style(theme='monokai', context='notebook', ticks=True, grid=False)
-from time import time
-
 
 CROSS_SECTIONAL = 'oasis_cross-sectional.csv'
 LONGITUDINAL = 'oasis_longitudinal.csv'
@@ -119,161 +115,44 @@ data_cs.describe()
 # %%
 ############################# RANDOM FORESTS #################################
 #  %%
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import explained_variance_score, precision_score
 
-# The CDR column contains the target information.
-train_me = ['M/F', 'Age', 'EDUC', 'SES', 'MMSE', 'eTIV', 'nWBV', 'ASF','CDR']
+best_score = 0
+for c_paramter in [0.001, 0.01, 0.1, 1, 10, 100, 1000]: #iterate over the values we need to try for the parameter C
+    for gamma_paramter in [0.001, 0.01, 0.1, 1, 10, 100, 1000]: #iterate over the values we need to try for the parameter gamma
+        for k_parameter in ['rbf', 'linear', 'poly', 'sigmoid']: # iterate over the values we need to try for the kernel parameter
+            svmModel = SVC(kernel=k_parameter, C=c_paramter, gamma=gamma_paramter) #define the model
+            # perform cross-validation
+            scores = cross_val_score(svmModel, X_trainval_scaled, Y_trainval, cv=kfolds, scoring='accuracy')
+            # the training set will be split internally into training and cross validation
 
-l_train, l_test = train_test_split(data_l[train_me])
-cs_train, cs_test = train_test_split(data_cs[train_me])
-
-
-#%%
-
-class EnsembleTest:
-    """ Test multiple model performance """
-    
-    def __init__(self, X_train, y_train, X_test, y_test, _type='regression'):
-        """
-        initialize data and type of problem
-        :param X_train:
-        :param y_train:
-        :param X_test:
-        :param y_test:
-        :param _type: regression or classification
-        """
-        self.scores = {}
-        self.execution_time = {}
-        self.metric = {}
-        self.X_train = X_train
-        self.y_train = y_train
-        self.X_test = X_test
-        self.y_test = y_test
-        self.type = _type
-        if _type == 'regression':
-            self.score_name = 'R^2 score' 
-            self.metric_name = 'Explained variance' 
-        else:
-            self.score_name = 'Mean accuracy score'
-            self.metric_name = 'Precision'
+            # compute mean cross-validation accuracy
+            score = np.mean(scores)
+            # if we got a better score, store the score and parameters
+            if score > best_score:
+                best_score = score #store the score 
+                best_parameter_c = c_paramter #store the parameter c
+                best_parameter_gamma = gamma_paramter #store the parameter gamma
+                best_parameter_k = k_parameter
             
-    def fit_model(self, model, name):
-        """
-        TODO:
-        - Fit the model on train data.
-        - Store execution time required to fit.
-        - Store scores on test data
-        - Predict on test data
-        
-        Each model passed as parameter has member functions of the following form:
-          model.fit(x_train, y_train)
-          model.score(x_test, y_test) 
-          model.predict(x_test)
-        
-        :param model: model
-        :param name: name of model
-        """
-        
-        # YOUR CODE HERE
-        t_start = time()
-        model.fit(X_train,y_train)
-        t_end = time()
 
-        self.execution_time[name] = t_end-t_start
-        self.scores[name] = model.score(X_test, y_test) 
-        predict = model.predict(X_test)
-        
-        if self.type == 'regression':
-            self.metric[name] = explained_variance_score(self.y_test, predict)
-        elif self.type == 'classification':
-            self.metric[name] = precision_score(self.y_test, predict)
+# rebuild a model with best parameters to get score 
+SelectedSVMmodel = SVC(C=best_parameter_c, gamma=best_parameter_gamma, kernel=best_parameter_k).fit(X_trainval_scaled, Y_trainval)
 
-    def print_result(self):
-        """
-            print results for all models trained and tested.
-        """
-        models_cross = pd.DataFrame({
-            'Model'         : list(self.metric.keys()),
-             self.score_name     : list(self.scores.values()),
-             self.metric_name    : list(self.metric.values()),
-            'Execution time': list(self.execution_time.values())})
-        print(models_cross.sort_values(by=self.score_name, ascending=False))
+test_score = SelectedSVMmodel.score(X_test_scaled, Y_test)
+PredictedOutput = SelectedSVMmodel.predict(X_test_scaled)
+test_recall = recall_score(Y_test, PredictedOutput, pos_label=1)
+fpr, tpr, thresholds = roc_curve(Y_test, PredictedOutput, pos_label=1)
+test_auc = auc(fpr, tpr)
+print("Best accuracy on cross validation set is:", best_score)
+print("Best parameter for c is: ", best_parameter_c)
+print("Best parameter for gamma is: ", best_parameter_gamma)
+print("Best parameter for kernel is: ", best_parameter_k)
+print("Test accuracy with the best parameters is", test_score)
+print("Test recall with the best parameters is", test_recall)
+print("Test recall with the best parameter is", test_auc)
 
-    def plot_metric(self):
-        """
-         There are 3 metrics for each type of experiment: 
-             time, metric score, scores
-         Produce a bar graph for each of the above metrics
-         Each bar graph should have results from all experiments 
-             of the same type side by side
-         
-         Note: The Metric name and score name depend on the type of experiment 
-             (regression or classification) 
-        """
-        # YOUR CODE HERE
-        fig = plt.figure()
-        ax = fig.add_axes([0,0,1,1])
-        exp_names = [key for key in self.execution_time]
-        exp_times = [self.execution_time[key] for key in exp_names]
-        ax.bar(exp_names,exp_times)
-        ax.set_xlabel('Experiment')
-        ax.set_ylabel('Execution Times')
-        ax.title.set_text('Experiment Execution Times')
-        ax.grid()
-
-        fig1 = plt.figure()
-        ax1 = fig1.add_axes([0,0,1,1])
-        exp_scores = [self.scores[key] for key in exp_names]
-        ax1.bar(exp_names,exp_scores)
-        ax1.set_xlabel('Experiment')
-        ax1.set_ylabel('Scores')
-        ax1.title.set_text('Experiment Scores')
-        ax1.grid()
-
-        fig2 = plt.figure()
-        ax2 = fig2.add_axes([0,0,1,1])
-        exp_metscore = [self.metric[key] for key in exp_names]
-        ax2.bar(exp_names,exp_metscore)
-        ax2.set_xlabel('Experiment')
-        ax2.set_ylabel('Metric Scores')
-        ax2.title.set_text('Experiment Metric Scores')
-        ax2.grid()
-
-#L_trianing data split
-y_train = l_train[['CDR']]
-del l_train['CDR']
-
-X_train = l_train
-
-# L_testing data split
-y_test = l_test[['CDR']]
-del l_test['CDR']
-
-X_test = l_test
-
-# # CS_training data split
-# y_train = l_train[['CDR']]
-# del l_train['CDR']
-
-# X_train = l_train
-# %%
-l_handler = EnsembleTest(
-    X_train, y_train, X_test, y_test, _type='regression')
-
-l_rf = RandomForestRegressor(n_estimators=100)
-l_rf.fit(X_train,y_train)
-
-
-
-#%%
-from sklearn.metrics import f1_score
-
-y_hat_test = l_rf.predict(X_test)
-met = f1_score(y_test, y_hat_test)
-
-
+m = 'SVM'
+acc.append([m, test_score, test_recall, test_auc, fpr, tpr, thresholds])
 
 ################################# SVM ########################################
 # %%
